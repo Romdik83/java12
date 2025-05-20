5a1fae2dc27a0e2eb7c227e81cccf65a25ac97a4
Romdik <romdik83@gmail.com>
2025-05-20 23:00:30 +0300

    Radio

--- src/main/java/ru/netology/stats/java12/service/Radio.java
+++ src/main/java/ru/netology/stats/java12/service/Radio.java
@@ -1,5 +1,12 @@
 package ru.netology.stats.java12.service;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class Radio {
 
     private int maxStation = 9;
